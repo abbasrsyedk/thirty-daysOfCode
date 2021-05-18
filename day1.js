40e715afcd582a5d8889b079a797920b3454625c
R.S.K.Abbas <rskabbas@outlook.com>
2021-05-17 23:33:03 -0700

    Add files via upload

---
+++ day1.js
@@ -0,0 +1,9 @@
+var myInt, myFloat, myString;
+    myInt = parseInt(readLine());
+    myFloat = parseFloat(readLine());
+    myString = readLine();
+
+    console.log(i + myInt);
+    console.log((d + myFloat).toFixed(1));
+    console.log(s + myString)
+}
