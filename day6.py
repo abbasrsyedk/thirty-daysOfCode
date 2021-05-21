e955d3fb8547ebaf2ff198f548e41b3a291e76b8
R.S.K.Abbas <rskabbas@outlook.com>
2021-05-21 03:58:55 -0700

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,7 @@
+inp = int(input().strip())
+for _ in range(inp):
+    s = input().strip()
+    n = len(s)
+    even = [s[i] for i in range(0, n, 2)]  # list of even chars
+    odd = [s[i] for i in range(1, n, 2)]  # list of odd chars
+    print(''.join(even), ''.join(odd))
